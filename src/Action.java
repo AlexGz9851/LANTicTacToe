af554889bebc6e9ba42b2f54e8887dc9c917d87c
Iván Morett <luisivanmorett@gmail.com>
2018-04-24 15:56:08 -0500

    Enum ready
    You're free to add or change enums that you will need

--- src/Action.java
+++ src/Action.java
@@ -1,4 +1,23 @@
 
 public enum Action {
-
+	USERLIST("userList"), LOGOUT("logOut"), NEWUSER("newUser"), ERROR("error"), INICIOJUEGO("inicioJuego"), FINJUEGO("finJuego"), TURNO("turno");
+	private String value;
+	
+	private Action(String value) {
+		this.value = value;
+	}
+	
+	@Override
+    public String toString(){
+        return this.value;
+    }
+    
+    public static Action getValue(String text) {
+    	for (Action b : Action.values()) {
+			if (b.toString().equalsIgnoreCase(text)) {
+				return b;
+			}
+    	}
+    	throw new IllegalArgumentException("No enum for \""+text+"\"");
+    }
 }
